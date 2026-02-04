ce2d27567daf6969e974ffc6673d80ce66985e2a
harshshrivas8055 <harshkumarshrivas128@gmail.com>
2026-02-04 12:26:37 +0530

    pac

--- src/main.tsx
+++ src/main.tsx
@@ -1,5 +1,6 @@
 import { createRoot } from "react-dom/client";
 import App from "./App.tsx";
+import { Analytics } from "@vercel/analytics/react"
 import "./index.css";
 
 createRoot(document.getElementById("root")!).render(<App />);
